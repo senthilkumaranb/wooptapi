0b4b8f3b2091dda4786c6f9ba6a9b2461afbf65a
senthilkumaranb <senthilkb@in.ibm.com>
2016-03-02 23:07:17 +0530

    removed shopid

--- src/main/java/com/woopt/api/model/ShopLoyaltyProgram.java
+++ src/main/java/com/woopt/api/model/ShopLoyaltyProgram.java
@@ -4,16 +4,13 @@ public class ShopLoyaltyProgram {
 	
 	private int shopLoyaltyProgramId;
 	
-    private int shopId;
-    
     private String shopLoyaltyProgramName;
     
     private String shopLoyaltyProgramType;
     
     private int shopLoyaltyProgramNoOfStages;
     
-    private ShopLoyaltyProgramStage[] shopLoyaltyProgramStage;
-    
     private ShopLoyaltyProgramStage shopLoyaltyProgramCurrentStage;
 
+    private ShopLoyaltyProgramStage[] shopLoyaltyProgramStage;
 }
