542a33928284b873d576fab8bc97de89c5a2d10c
senthilkumaranb <senthilkb@in.ibm.com>
2016-03-04 23:54:35 +0530

    Updated variables

--- src/main/java/com/woopt/api/model/Order.java
+++ src/main/java/com/woopt/api/model/Order.java
@@ -3,13 +3,13 @@ package com.woopt.api.model;
 public class Order {
 	
 
-	  private int user_shop_order_id;
-	  private int user_id;
-	  private int shop_id;
-	  private int user_shop_order_total_transaction;
-	  private int user_shop_order_total_redeemed;
-	  private int user_shop_order_status;
-	  private int user_shop_order_net_transaction;
+	  private int userShopOrderId;
+	  private int userId;
+	  private int shopId;
+	  private int userShopOrderTotalTransaction;
+	  private int userShopOrderTotalRedeemed;
+	  private int userShopOrderStatus;
+	  private int userShopOrderNetTransaction;
 
 	  private OrderCartOfferAdded[] orderCartOfferAdded;
 	  
--- src/main/java/com/woopt/api/model/OrderCartOfferAdded.java
+++ src/main/java/com/woopt/api/model/OrderCartOfferAdded.java
@@ -2,7 +2,7 @@ package com.woopt.api.model;
 
 public class OrderCartOfferAdded {
 	
-	private UserDealView userDealViewModel;
-	private int user_shop_order_cart_offer_status;
+	private UserDealView userDeal;
+	private int userShopOrderCartOfferStatus;
 
 }
