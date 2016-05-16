dca3cc49dc2ceb4bc04de181700668315e0b31db
senthilkumaranb <senthilkumaranb@gmail.com>
2016-05-16 22:55:22 +0530

    Offer Publish Class added

---
+++ src/main/java/com/woopt/api/dao/OfferUserPublishDAO.java
@@ -0,0 +1,23 @@
+package com.woopt.api.dao;
+
+import java.util.List;
+
+import com.woopt.api.entity.OfferUserPublishEntity;
+
+public interface OfferUserPublishDAO {
+	
+	public OfferUserPublishEntity save(OfferUserPublishEntity offerUserPublishEntity);
+	
+	public OfferUserPublishEntity  update(OfferUserPublishEntity offerUserPublishEntity);
+	
+	public void  delete(int offerUserPublishId);
+	
+	public List<OfferUserPublishEntity> list();
+	
+	public List<OfferUserPublishEntity> getbyOfferId(int offerId);
+	
+	public List<OfferUserPublishEntity> getbyUserId(int shopId);
+	
+	public OfferUserPublishEntity findById(int offerUserPublishId);
+
+}
